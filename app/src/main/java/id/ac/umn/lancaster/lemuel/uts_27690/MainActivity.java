3025e6f03dc09e102853303c4833e76040f56519
Lemuel Lancaster <32617101+Itsakaseru@users.noreply.github.com>
2021-03-08 13:54:44 +0700

    Project init.

---
+++ app/src/main/java/id/ac/umn/lancaster/lemuel/uts_27690/MainActivity.java
@@ -0,0 +1,14 @@
+package id.ac.umn.lancaster.lemuel.uts_27690;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
